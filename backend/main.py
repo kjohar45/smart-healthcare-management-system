01df51387c07f26bf1724ceae558206549a88ccd
Karuna Johar <149220669+kjohar45@users.noreply.github.com>
2026-01-05 18:41:01 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Smart Healthcare Management System")
 
 @app.get("/")
 def read_root():
     return {"message": "Backend is running"}
-
