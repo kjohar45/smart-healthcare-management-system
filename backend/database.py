a9ecbef7054bdb700f878b556d0655b55cc0e594
Karuna Johar <149220669+kjohar45@users.noreply.github.com>
2026-01-05 18:39:51 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://username:password@localhost:5432/healthcare_db"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 
