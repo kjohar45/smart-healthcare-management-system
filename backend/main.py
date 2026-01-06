dea6e7c31cf39b5f6a7fd18169993f134cc50f4f
Karuna Johar <149220669+kjohar45@users.noreply.github.com>
2026-01-06 18:00:02 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+from sqlalchemy.orm import Session
+from backend.database import SessionLocal
+from backend import models
 from fastapi import FastAPI
 
 app = FastAPI(title="Smart Healthcare Management System")
@@ -5,3 +8,42 @@ app = FastAPI(title="Smart Healthcare Management System")
 @app.get("/")
 def read_root():
     return {"message": "Backend is running"}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+from fastapi import Depends
+
+@app.post("/users")
+def create_user(name: str, email: str, password: str, db: Session = Depends(get_db)):
+    user = models.User(name=name, email=email, password=password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+@app.post("/appointments")
+def book_appointment(user_id: int, doctor_id: int, db: Session = Depends(get_db)):
+    appointment = models.Appointment(
+        user_id=user_id,
+        doctor_id=doctor_id,
+        status="Scheduled"
+    )
+    db.add(appointment)
+    db.commit()
+    db.refresh(appointment)
+    return appointment
+@app.post("/payments")
+def make_payment(appointment_id: int, amount: float, method: str, db: Session = Depends(get_db)):
+    payment = models.Payment(
+        appointment_id=appointment_id,
+        amount=amount,
+        payment_method=method
+    )
+    db.add(payment)
+    db.commit()
+    db.refresh(payment)
+    return payment
+
